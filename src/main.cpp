d78f4a36cb064fa1032454cd133d3d6d0e6c5c58
NiklasWeinreich <niklasweinreichhansen@hotmail.com>
2023-06-19 13:06:38 +0200

    graf implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,231 @@
-#include <Arduino.h>
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com  
+*********/
 
-// put function declarations here:
-int myFunction(int, int);
+// Import required libraries
+#ifdef ESP32
+  #include <WiFi.h>
+  #include <ESPAsyncWebServer.h>
+#else
+  #include <Arduino.h>
+  #include <ESP8266WiFi.h>
+  #include <Hash.h>
+  #include <ESPAsyncTCP.h>
+  #include <ESPAsyncWebServer.h>
+#endif
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Data wire is connected to GPIO 4
+#define ONE_WIRE_BUS 4
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+// Variables to store temperature values
+String temperatureF = "";
+String temperatureC = "";
+
+// Timer variables
+unsigned long lastTime = 0;  
+unsigned long timerDelay = 30000;
+
+// Replace with your network credentials
+const char* ssid = "E308";
+const char* password = "98806829";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+String readDSTemperatureC() {
+  // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
+  sensors.requestTemperatures(); 
+  float tempC = sensors.getTempCByIndex(0);
+
+  if(tempC == -127.00) {
+    Serial.println("Failed to read from DS18B20 sensor");
+    return "--";
+  } else {
+    Serial.print("Temperature Celsius: ");
+    Serial.println(tempC); 
+  }
+  return String(tempC);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+String readDSTemperatureF() {
+  // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
+  sensors.requestTemperatures(); 
+  float tempF = sensors.getTempFByIndex(0);
+
+  if(int(tempF) == -196){
+    Serial.println("Failed to read from DS18B20 sensor");
+    return "--";
+  } else {
+    Serial.print("Temperature Fahrenheit: ");
+    Serial.println(tempF);
+  }
+  return String(tempF);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <style>
+    html {
+     font-family: Arial;
+     display: inline-block;
+     margin: 0px auto;
+     text-align: center;
+    }
+    h2 { font-size: 3.0rem; }
+    p { font-size: 3.0rem; }
+    .units { font-size: 1.2rem; }
+    .ds-labels{
+      font-size: 1.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+    canvas{
+      -moz-user-select: none;
+      -webkit-user-select: none;
+      -ms-user-select: none;
+    }
+  </style>
+</head>
+<body>
+  <h2>Temperature Monitor</h2>
+  <canvas id="temperatureChart"></canvas>
+  <script>
+    var temperatureChart = new Chart(document.getElementById('temperatureChart'), {
+      type: 'line',
+      data: {
+        labels: [],
+        datasets: [{
+          label: 'Temperature (°C)',
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          lineTension: 0.1,
+          data: [],
+        }]
+      },
+      options: {
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Time'
+            }
+          },
+          y: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Temperature (°C)'
+            },
+            suggestedMin: 0,
+            suggestedMax: 50
+          }
+        },
+        plugins: {
+          legend: {
+            display: false
+          }
+        }
+      }
+    });
+
+    function addData(chart, label, data) {
+      chart.data.labels.push(label);
+      chart.data.datasets[0].data.push(data);
+      chart.update();
+    }
+
+    function removeOldData(chart) {
+      if (chart.data.labels.length > 10) {
+        chart.data.labels.shift();
+        chart.data.datasets[0].data.shift();
+        chart.update();
+      }
+    }
+
+    setInterval(function() {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var tempC = parseFloat(this.responseText);
+          var now = new Date();
+          var time = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+          addData(temperatureChart, time, tempC);
+          removeOldData(temperatureChart);
+        }
+      };
+      xhttp.open("GET", "/temperaturec", true);
+      xhttp.send();
+    }, 10000);
+  </script>
+</body>
+</html>)rawliteral";
+
+// Replaces placeholder with DS18B20 values
+String processor(const String& var){
+  if(var == "TEMPERATUREC"){
+    return temperatureC;
+  }
+  else if(var == "TEMPERATUREF"){
+    return temperatureF;
+  }
+  return String();
+}
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  Serial.println();
+  
+  // Start up the DS18B20 library
+  sensors.begin();
+
+  temperatureC = readDSTemperatureC();
+  temperatureF = readDSTemperatureF();
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  
+  // Print ESP Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.on("/temperaturec", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", temperatureC.c_str());
+  });
+  server.on("/temperaturef", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", temperatureF.c_str());
+  });
+  // Start server
+  server.begin();
+}
+
+void loop(){
+  if ((millis() - lastTime) > timerDelay) {
+    temperatureC = readDSTemperatureC();
+    temperatureF = readDSTemperatureF();
+    lastTime = millis();
+  }  
 }
